dc49d170ab24bed1b05bfce55db2835337e7211f
”昨前进明“ <455664949@qq.com>
2025-05-06 23:26:22 +0800

    修复分页不成功的问题

--- src/main/java/com/example/onlineexam/req/PageReq.java
+++ src/main/java/com/example/onlineexam/req/PageReq.java
@@ -3,13 +3,14 @@ package com.example.onlineexam.req;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public class PageReq {
     @NotNull(message = "【页码】不能为空")
     private int page;
 
     @NotNull(message = "【每页条数】不能为空")
-    @Max(value = 500, message = "【每页条数】不能超过50")
+    @Max(value = 50, message = "【每页条数】不能超过50")
     private int size;
 
     public int getPage() {
